6d8e2780ccd2307afb410450f05142ff0958b055
alex <yanalex981@users.noreply.github.com>
2023-06-24 20:40:07 -0400

    can play sound on keypress now

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,16 +1,19 @@
 let keys = new Map(
     [... document.getElementsByClassName('key')]
         .map(item => [item.dataset.key, item]));
-console.log(keys);
+
 let audio = new Map(
     [... document.getElementsByTagName('audio')]
         .map(item => [item.dataset.key, item]));
-console.log(audio);
 
-// window.addEventListener('keydown',
-//     function (event) {
-//         console.log(event);
-
-//         audio[1].play();
-//     }
-// );
+window.addEventListener('keydown',
+    function (event) {
+        let key = keys.get(event.key);
+        let sound = audio.get(event.key);
+        if (!sound || !key)
+            return;
+        
+        
+        sound.play();
+    }
+);
