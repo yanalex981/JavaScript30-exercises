0d9d3f5a0838d423ef7b2b863bb295eb83494713
alex <yanalex981@users.noreply.github.com>
2023-06-25 21:33:31 -0400

    fixed the jerking when the second hand wraps around 360 degrees

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -2,25 +2,50 @@ const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
-function syncDate() {
-    const currentTime = new Date();
-    const hours = currentTime.getHours();
-    const minutes = currentTime.getMinutes();
-    const seconds = currentTime.getSeconds();
+const DEGREES_PER_HOUR = 360.0 / 12.0;
+const DEGREES_PER_TICK = 360.0 / 60.0;
+const ANGLE_OFFSET = -90.0;
 
-    const DEGREES_PER_HOUR = 360.0 / 12.0;
-    const DEGREES_PER_TICK = 360.0 / 60.0;
-    const ANGLE_OFFSET = -90.0;
-
-    const hourAngle = (DEGREES_PER_HOUR*hours + minutes/60.0*DEGREES_PER_HOUR + ANGLE_OFFSET) % 360;
-    const minutesAngle = (DEGREES_PER_TICK*minutes + DEGREES_PER_TICK*seconds/60.0 + ANGLE_OFFSET) % 360;
-    const secondsAngle = (DEGREES_PER_TICK*seconds) % 360;
-
-    hourHand.style.transform = `rotate(${hourAngle}deg)`;
-    minuteHand.style.transform = `rotate(${minutesAngle}deg)`;
-    secondHand.style.transform = `rotate(${secondsAngle}deg)`;
+function setHoursAngle(angle) {
+    hourHand.style.transform = `rotate(${angle}deg)`;
 }
 
-syncDate();
+function setMinutesAngle(angle) {
+    minuteHand.style.transform = `rotate(${angle}deg)`;
+}
 
-setInterval(syncDate, 500);
+function setSecondsAngle(angle) {
+    secondHand.style.transform = `rotate(${angle}deg)`;
+}
+
+function getHoursAngle(hours, minutes) {
+    return DEGREES_PER_HOUR*hours + minutes/60.0*DEGREES_PER_HOUR;
+}
+
+function getMinutesAngle(minutes, seconds) {
+    return DEGREES_PER_TICK*minutes + DEGREES_PER_TICK*seconds/60.0;
+}
+
+function getSecondsAngle(seconds) {
+    return DEGREES_PER_TICK*seconds;
+}
+
+let prevDate = new Date();
+let currentDate = new Date();
+
+let secondsAngle = getSecondsAngle(currentDate.getSeconds()) + ANGLE_OFFSET;
+
+function advanceHands() {
+    prevDate = currentDate;
+    currentDate = new Date();
+    const timeDiff = currentDate - prevDate;
+    const seconds = timeDiff / 1000.0;
+    setHoursAngle(getHoursAngle(currentDate.getHours(), currentDate.getMinutes()) + ANGLE_OFFSET);
+    setMinutesAngle(getMinutesAngle(getMinutesAngle(currentDate.getMinutes(), currentDate.getSeconds()) + ANGLE_OFFSET));
+    secondsAngle += getSecondsAngle(seconds);
+    setSecondsAngle(secondsAngle + ANGLE_OFFSET);
+}
+
+setInterval(() => {
+    advanceHands();
+}, 1000);
