f98dc36f216e37f4e3841dc80c7bbfa37c92f921
alex <yanalex981@users.noreply.github.com>
2023-06-27 03:48:30 -0400

    the smaller text now enter and leave after flex-grow/shrink

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,9 +1,15 @@
 const panels = document.querySelectorAll('.panel');
 
 function handleOnClick() {
-    this.style.flex = this.style.flex? '' : '4';
+    this.classList.toggle('open');
+}
+
+function handleTransitionEnd(event) {
+    if (event.propertyName !== 'flex-grow') return;
+    this.classList.toggle('active');
 }
 
 panels.forEach((node, key, parent) => {
     node.addEventListener('click', handleOnClick);
+    node.addEventListener('transitionend', handleTransitionEnd);
 });
