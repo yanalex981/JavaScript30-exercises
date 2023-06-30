83367d437fd23b0b27296700d20ebd5d2af6ac55
alex <yanalex981@users.noreply.github.com>
2023-06-30 00:13:10 -0400

    mostly finished fine tuning the inky style, everything's looking pretty good

--- 08 - Fun with HTML5 Canvas/script.js
+++ 08 - Fun with HTML5 Canvas/script.js
@@ -6,20 +6,14 @@ const dpiScale = window.devicePixelRatio || 1;
 [canvas.width, canvas.height] = [rect.width * dpiScale, rect.height * dpiScale];
 
 context.strokeStyle = '#000000';
-// context.lineWidth = 1;
 context.lineJoin = 'round';
 context.lineCap = 'round';
 
-// context.beginPath();
-// context.moveTo(0, 0);
-// context.lineTo(canvas.width, canvas.height);
-// context.stroke();
-
 const ZERO = { x: 0, y: 0 };
-const SAMPLE_PERIOD = 1000 / 120;
-const MAX_BRUSH_SIZE = 32;
-const MIN_BRUSH_SIZE = 4;
-const BUFFER_SIZE = 8;
+const SAMPLE_PERIOD = 1000 / 144;
+const MAX_BRUSH_SIZE = 12;
+const MIN_BRUSH_SIZE = 2;
+const BUFFER_SIZE = 6;
 
 let drawing = false;
 
@@ -30,33 +24,22 @@ let mousePosition = ZERO
 let velocity = 0;
 let brushIntensity = 1;
 
-canvas.addEventListener('mousedown', (event) => {
-    drawing = true;
+canvas.addEventListener('pointerdown', (event) => {
     mousePosition = { x: event.offsetX, y: event.offsetY };
+    drawing = true;
     posBuffer = [mousePosition];
     lineStart = mousePosition;
     lineEnd = mousePosition;
     velocity = 0;
 });
 
-canvas.addEventListener('mouseup', (event) => {
-    drawing = false;
+canvas.addEventListener('pointerup', (event) => {
     mousePosition = { x: event.offsetX, y: event.offsetY };
+    drawing = false;
 });
 
-canvas.addEventListener('mousemove', (event) => {
+canvas.addEventListener('pointermove', (event) => {
     mousePosition = { x: event.offsetX, y: event.offsetY };
-    // if (!drawing) return;
-
-    // // console.log(event);
-    // posBuffer.shift();
-    // posBuffer.push({ x: event.offsetX, y: event.offsetY });
-
-    // console.log(posBuffer);
-
-    // // velocity = posBuffer.forEach(value => {
-    // //     //
-    // // });
 });
 
 function* bubble(arr) {
@@ -85,17 +68,16 @@ function takeSamples() {
 
     lineStart = lineEnd;
     lineEnd = posBuffer.reduce((sum, pos) => ({ x: pos.x / posBuffer.length + sum.x, y: pos.y / posBuffer.length + sum.y }), { x: 0, y: 0 });
-    // console.log(lineEnd);
 
     velocity = [... lengthsOf(posBuffer)].reduce((total, value) => total + value, 0);
     
-    brushIntensity = velocity > 250? MIN_BRUSH_SIZE : 7/14400*(velocity - 250) ** 2 + MIN_BRUSH_SIZE;
+    brushIntensity = velocity > 60? MIN_BRUSH_SIZE : 1/21600*(60 - velocity) ** 3 + MIN_BRUSH_SIZE;
 }
 
 setInterval(() => {
     if (!drawing) return;
     takeSamples();
-    context.lineWidth = brushIntensity;
+    context.lineWidth = clamp(brushIntensity, MIN_BRUSH_SIZE, MAX_BRUSH_SIZE);
     context.beginPath();
     context.moveTo(lineStart.x, lineStart.y);
     context.lineTo(lineEnd.x, lineEnd.y);
